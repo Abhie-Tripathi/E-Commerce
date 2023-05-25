42a08f1c32872fc5460495578ce1b6a6b8d15f3f
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-05-25 20:40:26 +0530

    Added Some buttons

--- src/components/Itemdescription.js
+++ src/components/Itemdescription.js
@@ -50,55 +50,95 @@ const ItemDescription = ({ item }) => {
   const { id } = useParams();
   const selecteditem = productsArr.find((item) => item.title === id);
 
-  return (<><Types/>
-    <Container className="mt-5">
-      <Row>
-        <Col
-          md={6}
-          className="d-flex align-items-center justify-content-center"
-        >
-          <img
-            src={selecteditem.imageUrl}
-            alt="Product"
-            style={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              objectFit: "contain",
-            }}
-          />
-        </Col>
-        <Col md={6}>
-          <Card.Body>
-            <Card.Text
+  return (
+    <>
+      <Types />
+      <Container className="mt-5">
+        <Row>
+          <Col
+            md={6}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <img
+              src={selecteditem.imageUrl}
+              alt="Product"
               style={{
-                display: "flex",
-                justifyContent: "center",
-                fontSize: "32px",
-                color: "white",
-                fontWeight: 500,
+                maxWidth: "100%",
+                maxHeight: "100%",
+                objectFit: "contain",
               }}
-            >
-              {selecteditem.title}
-            </Card.Text>
-            <p style={{ color: "white",display: "flex",
-                justifyContent: "center" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut
-              enim id libero lacinia congue ac nec turpis. Integer eu arcu ut ex
-              ultricies euismod. Sed id bibendum tortor. Etiam eget justo ut
-              augue tincidunt aliquet. Maecenas id nisi a felis fringilla
-              varius. Nulla id tristique ligula, vitae luctus lacus. Morbi
-              aliquet diam sed nisl accumsan, id condimentum velit malesuada.
-              Nulla facilisi.
-            </p>
-            <p style={{
-                fontSize: "29px",
-                color: "white",
-                fontWeight: 500,
-              }}>{`Price : ${selecteditem.price}`}</p>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Container>
+            />
+          </Col>
+          <Col md={6}>
+            <Card.Body>
+              <Card.Text
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  fontSize: "32px",
+                  color: "white",
+                  fontWeight: 500,
+                }}
+              >
+                {selecteditem.title}
+              </Card.Text>
+              <p
+                style={{
+                  color: "white",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                ut enim id libero lacinia congue ac nec turpis. Integer eu arcu
+                ut ex ultricies euismod. Sed id bibendum tortor. Etiam eget
+                justo ut augue tincidunt aliquet. Maecenas id nisi a felis
+                fringilla varius. Nulla id tristique ligula, vitae luctus lacus.
+                Morbi aliquet diam sed nisl accumsan, id condimentum velit
+                malesuada. Nulla facilisi.
+              </p>
+              <p
+                style={{
+                  fontSize: "29px",
+                  color: "white",
+                  fontWeight: 500,
+                }}
+              >{`Price : ${selecteditem.price}`}</p>
+              <button
+                style={{
+                  background: "chocolate",
+                  border: "none",
+                  borderRadius: "13px",
+                  padding: "19px",
+                  width: "212px",
+                  height: "71px",
+                  fontSize: "24px",
+                  color: "white",
+                  fontWeight: 550,
+                }}
+              >
+                Buy Now
+              </button>
+              <button
+                style={{
+                  background: "cornflowerblue",
+                  border: "none",
+                  borderRadius: "13px",
+                  padding: "19px",
+                  width: "212px",
+                  height: "71px",
+                  fontSize: "24px",
+                  color: "white",
+                  fontWeight: 550,
+                  marginLeft: "104px",
+                }}
+              >
+                Add To Cart
+              </button>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
